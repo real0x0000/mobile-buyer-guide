e6f97452597c518b0c0b00bb26b79ddb79611ea3
Anuwat Sittichak <real@Admins-MacBook-Air-3.local>
2018-07-05 17:42:59 +0700

    Add xcode project.

---
+++ Mobile-buyer-guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mobile-buyer-guide
+//
+//  Created by Anuwat Sittichak on 5/7/2561 BE.
+//  Copyright © 2561 real0x0000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mobile-buyer-guideTests/Mobile_buyer_guideTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mobile_buyer_guideTests.swift
+//  Mobile-buyer-guideTests
+//
+//  Created by Anuwat Sittichak on 5/7/2561 BE.
+//  Copyright © 2561 real0x0000. All rights reserved.
+//
+
+import XCTest
+@testable import Mobile_buyer_guide
+
+class Mobile_buyer_guideTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
